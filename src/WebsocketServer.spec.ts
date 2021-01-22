c1465e261df5349497a1e43db021cfd56ff10851
Jayd Page <jayd.page@chillisoft.co.za>
2021-01-22 21:02:19 +0200

    Add example of how to wait on a mocked function to be called before running a test assertion

--- src/WebsocketServer.spec.ts
+++ src/WebsocketServer.spec.ts
@@ -1,7 +1,7 @@
 import { WebSocketServer } from "./WebsocketServer"
 
 describe('WebSocketServer', () => {
-  it('fails because test exits before callback is called', async () => {
+  it('fails - because test exits before callback is called', async () => {
     // Arrange
     const callback = jest.fn()
     const server = new WebSocketServer()
@@ -12,4 +12,26 @@ describe('WebSocketServer', () => {
     // Assert
     expect(callback).toBeCalledWith(payload)
   })
+
+  it('passes - because we wait for callback to be called before assertion', async () => {
+    // Arrange
+    const callback = jest.fn()
+    const server = new WebSocketServer()
+    const payload = { bar: 1 }
+    const callbackCalled = listenForCall(callback)
+    await server.on('foo', callback)
+    // Act
+    await server.receive('foo', payload)
+    // Assert
+    await callbackCalled
+    expect(callback).toBeCalledWith(payload)
+  })
 })
+
+async function listenForCall(callback: jest.Mock) {
+  return new Promise<void>(resolve => {
+    callback.mockImplementation(() => {
+      resolve()
+    })
+  })
+}
